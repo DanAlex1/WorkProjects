adf6d9d7b277057fd2b5c6b489747b4619cc849a
Daniel Vass <dan011@gmx.co.uk>
2023-03-10 11:57:34 +0000

    added more tests

--- __tests__/palindrome.test.js
+++ __tests__/palindrome.test.js
@@ -3,3 +3,7 @@ const isPalindrome = require('../src/palindrome')
 test('returns true for 121', () =>{
     expect(isPalindrome(121)).toBe(true)
 })
+
+test('returns false for -121', () =>{
+    expect(isPalindrome(-121)).toBe(false)
+})

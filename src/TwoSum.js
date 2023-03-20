130f9e419f9d759def347e3c1e75b820b7d7c125
Daniel Vass <dan011@gmx.co.uk>
2023-03-20 16:18:59 +0000

    20/03/2023

--- src/TwoSum.js
+++ src/TwoSum.js
@@ -1,12 +1,19 @@
-let nums=[2,7,11,15];
+let nums=[3,2,4];
 let target = 9;
 function twoSum(nums, target) {
-    for(i = 0; i < nums.length; i++){
+    for(i = 0; i <= nums.length; i++){
         if(nums[i] >= target){
-            nums.splice(i, 1);
+            nums.splice(i);
         }
     }
+    var sum = 0;
+   nums.forEach(function(z) {
+    sum += z;
+   })
+   console.log(`this ${sum}`)
+   if(sum = target){
     return(nums)
+   }
 };
 
-console.log(twoSum(nums, target))
+console.log(twoSum(nums, target));
